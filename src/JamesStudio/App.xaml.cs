ab209481205ffb92b8746c0661081a512686a5d4
Jaewung Lee <james@jamesnet.dev>
2024-11-29 07:09:25 +0800

    Updates

---
+++ src/JamesStudio/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace JamesStudio
+{
+    public sealed partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+
+            // Enter construction logic here...
+            JamesStudioBootstrapper jamesStudioBootstrapper = new JamesStudioBootstrapper();
+            jamesStudioBootstrapper.Run();
+
+            var mainPage = new MainPage();
+            Window.Current.Content = mainPage;
+        }
+    }
+}
---
+++ src/JamesStudio/JamesStudioBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Jamesnet.Core;
+using Studio.Main.UI.Views;
+
+namespace JamesStudio
+{
+    internal class JamesStudioBootstrapper : AppBootstrapper
+    {
+        protected override void RegisterDependencies(IContainer container)
+        {
+            container.RegisterSingleton<IView, MainContent>();
+        }
+
+        protected override void RegisterViewModels()
+        {
+        }
+
+        protected override void SettingsLayer(ILayerManager layer, IContainer container)
+        {
+            IView main = container.Resolve<MainContent>();
+
+            layer.Mapping("MAIN", main);
+        }
+    }
+}
